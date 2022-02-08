154dbb8de9ffaecc0e9ae4b0def62581d23d5254
dnjstjr93 <dnjstjr93@gmail.com>
2022-02-08 17:51:15 +0900

    Initial commit

--- lib_lx_cam.py
+++ lib_lx_cam.py
@@ -1,39 +1,82 @@
 #!/usr/bin/env python
 
-# python-gphoto2 - Python interface to libgphoto2
-# http://github.com/jim-easterbrook/python-gphoto2
-# Copyright (C) 2015-19  Jim Easterbrook  jim@jim-easterbrook.me.uk
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from __future__ import print_function
 
 import logging
-import os
-import subprocess
-import sys
 import datetime
+import json
+import sys
+import os
+import paho.mqtt.client as mqtt
 
 import gphoto2 as gp
 
 import ftplib
 
+my_lib_name = 'lib_lx_cam'
+
 camera = None
+ftp = None
+lib_mqtt_client = None
+
+control_topic = ''
+data_topic = ''
+
+broker_ip = 'localhost'
+port = 1883
+
+lib = dict()
+
+
+def on_connect(client, userdata, flags, rc):
+    global control_topic
+    global broker_ip
+
+    print('[msw_mqtt_connect] connect to ', broker_ip)
+    lib_mqtt_client.subscribe(control_topic, 0)
+    print('[lib]control_topic\n', control_topic)
+
+
+def on_disconnect(client, userdata, flags, rc=0):
+    print(str(rc))
+
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_message(client, userdata, msg):
+    global data_topic
+    global control_topic
+
+    message = str(msg.payload.decode("utf-8")).lower()
+    if message == 'g':
+        print('receive OFF')
+        control_msg = False
+
+
+def msw_mqtt_connect():
+    global lib_topic
+    global lib_mqtt_client
+    global broker_ip
+    global port
+
+    lib_topic = ''
+
+    lib_mqtt_client = mqtt.Client()
+    lib_mqtt_client.on_connect = on_connect
+    lib_mqtt_client.on_disconnect = on_disconnect
+    lib_mqtt_client.on_subscribe = on_subscribe
+    lib_mqtt_client.on_message = on_message
+    lib_mqtt_client.connect(broker_ip, port)
+    lib_mqtt_client.loop_start()
+
+    return lib_mqtt_client
 
 
 def action():
     global camera
+    global ftp
 
     file_name = (datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=9)).strftime('%Y-%m-%dT%H:%M:%S.%f')
 
@@ -49,41 +92,63 @@ def action():
     print('Copying image to', target)
     camera_file = camera.file_get(
         file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
-    camera_file.save('./image/' + target)
-
-    # subprocess.call(['xdg-open', target])
-
-    return target
-
-
-def main():
-    global camera
-
-    ftp = ftplib.FTP()
-    ftp.connect("203.253.128.177", 50023)
-    ftp.login("d_keti", "keti123")
-
-    # count = 0
-    #
-    # while (count < 10):
-    #     target = action()
-    #     sending_file = open(target, 'rb')
-    #     ftp.storbinary('STOR ' + '/Downloads/ftp_test/' + target, sending_file)
-    #     sending_file.close()
-    #
-    #     count += 1
+    camera_file.save(target)
 
     target = action()
     sending_file = open(target, 'rb')
     ftp.storbinary('STOR ' + '/Downloads/ftp_test/' + target, sending_file)
     sending_file.close()
 
-    ftp.close
-    camera.exit()
+    return target
 
 
+def main():
+    global camera
+    global lib_mqtt_client
+    global control_topic
+    global data_topic
+    global broker_ip
+    global port
+    global lib
+    global my_lib_name
 
-    return 0
+    my_msw_name = 'msw' + my_lib_name[3:] + '_' + 'msw' + my_lib_name[3:]
+
+    try:
+        lib = dict()
+        with open('./' + my_msw_name + '/' + my_lib_name + '.json', 'r') as f:
+            lib = json.load(f)
+            lib = json.loads(lib)
+
+    except Exception as e:
+        lib = dict()
+        lib["name"] = my_lib_name
+        lib["target"] = 'armv6'
+        lib["description"] = "[name]"
+        lib["scripts"] = './' + my_lib_name
+        lib["data"] = ['Status']
+        lib["control"] = ['Capture']
+        lib = json.dumps(lib, indent=4)
+        lib = json.loads(lib)
+
+        # with open('./' + my_msw_name + '/' + my_lib_name + '.json', 'w', encoding='utf-8') as json_file:
+        with open('./' + my_lib_name + '.json', 'w', encoding='utf-8') as json_file:
+            json.dump(lib, json_file, indent=4)
+
+    control_topic = '/MUV/control/' + lib["name"] + '/' + lib["control"][0]
+    data_topic = '/MUV/data/' + lib["name"] + '/' + lib["data"][0]
+
+    msw_mqtt_connect()
+
+    ftp = ftplib.FTP()
+    ftp.connect("203.253.128.177", 50023)
+    ftp.login("d_keti", "keti123")
+    #
+    # ftp.close
+    # camera.exit()
+
+    # return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
