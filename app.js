f25a80e936ee92ef28caf85d246c8e0ecf5a98a3
dnjstjr93 <dnjstjr93@gmail.com>
2022-04-22 17:31:34 +0900

    - 드론 이름 붙여서 폴더 생성하도록 수정

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const concurrently = require('concurrently');
 let success, failure = '';
 
 const { result } = concurrently([
-    'node captureImage.js',
+    'node captureImage.js ' + process.argv[3],
     'node geotagging.js',
     'node sendFTP.js ' + process.argv[2] + ' ' + process.argv[3],
 
