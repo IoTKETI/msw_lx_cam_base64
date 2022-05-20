bcff72f5b515e057154b4b0d0be7c53fad32ee5a
dnjstjr93 <dnjstjr93@gmail.com>
2022-05-20 17:43:56 +0900

    - 촬영 명령 받을 수 있도록 수정, FTP 전송 방식 및 시간 변경

--- sendFTP.js
+++ sendFTP.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 const moment = require("moment");
 const sendFTP = require("basic-ftp");
-const db = require('node-localdb');
+// const db = require('node-localdb');
 const {nanoid} = require("nanoid");
 const mqtt = require("mqtt");
 
-let gps_filename = db('./gps_filename.json');
+// let gps_filename = db('./gps_filename.json');
 
 const my_lib_name = 'lib_lx_cam';
 
-let prev_mission = 'Empty';
+// let prev_mission = 'Empty';
 let mission = '';
 let ftp_dir = '';
-let drone_name = '';
+let drone_name = process.argv[3];
 
 let ftp_client = null;
 let ftp_host = process.argv[2];
@@ -26,6 +26,7 @@ let lib = {};
 
 let lib_mqtt_client = null;
 let my_status_topic = '';
+let control_topic = '';
 
 let status = 'Init';
 let count = 0;
@@ -33,7 +34,7 @@ let count = 0;
 init();
 
 function init() {
-    read_mission();
+    // read_mission();
 
     ftp_connect(ftp_host, ftp_user, ftp_pw);
 
@@ -53,13 +54,12 @@ function init() {
     }
 
     my_status_topic = '/MUV/data/' + lib["name"] + '/' + lib["data"][2];
+    control_topic = '/MUV/control/' + lib["name"] + '/' + lib["control"][0];
 
-    lib_mqtt_connect('localhost', 1883);
-
-    send_image_via_ftp();
+    lib_mqtt_connect('localhost', 1883, control_topic);
 }
 
-function lib_mqtt_connect(broker_ip, port) {
+function lib_mqtt_connect(broker_ip, port, control) {
     if (lib_mqtt_client == null) {
         let connectOptions = {
             host: broker_ip,
@@ -78,12 +78,38 @@ function lib_mqtt_connect(broker_ip, port) {
         lib_mqtt_client = mqtt.connect(connectOptions);
 
         lib_mqtt_client.on('connect', function () {
-            console.log('[geotag_lib_mqtt_connect] connected to ' + broker_ip);
+            console.log('[ftp_lib_mqtt_connect] connected to ' + broker_ip);
+
+            if (control !== '') {
+                lib_mqtt_client.subscribe(control, function () {
+                    console.log('[ftp_lib_mqtt] lib_sub_control_topic: ' + control);
+                });
+            }
             lib_mqtt_client.publish(my_status_topic, status);
         });
 
         lib_mqtt_client.on('message', function (topic, message) {
-            console.log('From ' + topic + 'message is ' + message.toString());
+            if (topic === control) {
+                if (message.toString().includes('g')) {
+                    console.log(message.toString());
+                    let command_arr = message.toString().split(' ');
+                    mission = command_arr[2];
+
+                    ftp_dir = moment().format('YYYY-MM-DD') + '-' + mission + '_' + drone_name;
+                    !fs.existsSync(ftp_dir) && fs.mkdirSync(ftp_dir);
+
+                    ftp_client.ensureDir("/" + ftp_dir);
+                    console.log('[ftp_lib_mqtt] Create ( ' + ftp_dir + ' ) directory')
+
+                    count = 0;
+
+                    status = 'Start';
+                    lib_mqtt_client.publish(my_status_topic, status);
+                    send_image_via_ftp();
+                }
+            } else {
+                console.log('From ' + topic + 'message is ' + message.toString());
+            }
         });
 
         lib_mqtt_client.on('error', function (err) {
@@ -92,39 +118,39 @@ function lib_mqtt_connect(broker_ip, port) {
     }
 }
 
-function read_mission() {
-    try {
-        let argv = gps_filename.findOne({name: 'mission_name'})._settledValue;
-        if (argv === undefined) {
-            setTimeout(read_mission, 500);
-        } else {
-            if (argv.hasOwnProperty('mission')) {
-                mission = argv.mission;
-            } else {
-                mission = 'Empty';
-            }
-            if (argv.hasOwnProperty('drone')) {
-                drone_name = argv.drone;
-            } else {
-                drone_name = 'undefined';
-            }
-        }
-
-        ftp_dir = moment().format('YYYY-MM-DD') + '-' + argv.mission + '_' + argv.drone;
-
-        if (prev_mission !== mission) {
-            if (ftp_client !== null) {
-                ftp_client.ensureDir("/" + ftp_dir);
-            }
-            prev_mission = mission;
-        }
-    } catch (e) {
-        if (e instanceof TypeError) {
-            console.log(e)
-        }
-    }
-    setTimeout(read_mission, 1000);
-}
+// function read_mission() {
+//     try {
+//         let argv = gps_filename.findOne({name: 'mission_name'})._settledValue;
+//         if (argv === undefined) {
+//             setTimeout(read_mission, 500);
+//         } else {
+//             if (argv.hasOwnProperty('mission')) {
+//                 mission = argv.mission;
+//             } else {
+//                 mission = 'Empty';
+//             }
+//             if (argv.hasOwnProperty('drone')) {
+//                 drone_name = argv.drone;
+//             } else {
+//                 drone_name = 'undefined';
+//             }
+//         }
+//
+//         ftp_dir = moment().format('YYYY-MM-DD') + '-' + argv.mission + '_' + argv.drone;
+//
+//         if (prev_mission !== mission) {
+//             if (ftp_client !== null) {
+//                 ftp_client.ensureDir("/" + ftp_dir);
+//             }
+//             prev_mission = mission;
+//         }
+//     } catch (e) {
+//         if (e instanceof TypeError) {
+//             console.log(e)
+//         }
+//     }
+//     setTimeout(read_mission, 1000);
+// }
 
 async function ftp_connect(host, user, pw) {
     ftp_client = new sendFTP.Client(0)
@@ -137,9 +163,13 @@ async function ftp_connect(host, user, pw) {
             port: 50023
         })
 
-        ftp_client.ensureDir("/" + ftp_dir);
-
-        console.log('Connect FTP server to ' + host);
+        if (ftp_dir !== '') {
+            ftp_client.ensureDir("/" + ftp_dir);
+            console.log('Connect FTP server to ' + host);
+            console.log('Create ( ' + ftp_dir + ' ) directory')
+        } else {
+            console.log('Connect FTP server to ' + host);
+        }
     } catch (err) {
         console.log('[FTP] Error\n', err)
         console.log('FTP connection failed');
@@ -163,13 +193,12 @@ async function send_image_via_ftp() {
             }
         });
 
-        console.time('ftp');
-
         if (geotagged_arr.length > 0) {
+            console.time('ftp');
             await ftp_client.uploadFrom('./' + geotagging_dir + '/' + geotagged_arr[0], "/" + ftp_dir + '/' + geotagged_arr[0]).then(() => {
                 console.timeEnd('ftp');
-                console.log('send ' + geotagged_arr[0]);
-                setTimeout(move_image, 1000, './' + geotagging_dir + '/', './' + ftp_dir + '/', geotagged_arr[0]);
+                // console.log('send ' + geotagged_arr[0]);
+                setTimeout(move_image, 50, './' + geotagging_dir + '/', './' + ftp_dir + '/', geotagged_arr[0]);
                 status = 'Send ' + count++;
                 empty_count = 0;
                 lib_mqtt_client.publish(my_status_topic, status);
@@ -180,7 +209,7 @@ async function send_image_via_ftp() {
             } else if (status === 'empty') {
                 empty_count++;
             }
-
+            console.log(empty_count)
             if (empty_count > 20) {
                 status = 'Finish ' + (count - 1);
                 empty_count = 0;
@@ -189,7 +218,7 @@ async function send_image_via_ftp() {
         }
     } catch (e) {
     }
-    setTimeout(send_image_via_ftp, 1000);
+    setTimeout(send_image_via_ftp, 50);
 }
 
 function move_image(from, to, image) {
